197b3ecc523e4911ffe2d7169bfa07437f7d8e31
Nastia <nastionus@gmail.com>
2026-08-09 01:15:34 +0200

    MongoDB

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
 
 $event = [
-    #ToDo
+        'action' => 'init',
 ];
 $channel = 'internal';
 $redis->publish($channel, json_encode($event));
@@ -15,14 +15,23 @@ $redis->publish($channel, json_encode($event));
 $uri = 'mongodb://127.0.0.1:27017';
 $uriOptions = ['ServerSelectionTimeoutMS' => 10000];
 $mongoClient = new MongoDB\Client($uri, $uriOptions);
-$mongoDB = $mongoClient->getDatabase("game_data");
-$chat = $mongoDB->selectCollection("/*ToDo*/");
+
+$err = '';
+try {
+    $mongoClient->selectDatabase('admin')->command(['ping' => 1]);
+    $err = 'good';
+
+    $mongoDB = $mongoClient->getDatabase("game_data");
+    $chat = $mongoDB->selectCollection("users");
+}catch (\Exception $e){
+    $err = 'error' . $e->getMessage();
+}
 // ToDo
 // $messages = $chat->find([], ['sort' => ['_id' => -1]]);
 ?>
 <html lang="en">
 <head>
-    <title><!-- ToDo --></title>
+    <title><!-- To1Do --></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.7.1.js"
             integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>

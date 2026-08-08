3c28342050ed5b3b88347062b9406f04f6e81c62
Nastia <nastionus@gmail.com>
2026-08-08 21:49:15 +0200

    Base files

--- public/index.php
+++ public/index.php
@@ -1,1 +1,35 @@
+<!DOCTYPE html>
 <?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+
+$event = [
+    #ToDo
+];
+$channel = 'internal';
+$redis->publish($channel, json_encode($event));
+
+$uri = 'mongodb://127.0.0.1:27017';
+$uriOptions = ['ServerSelectionTimeoutMS' => 10000];
+$mongoClient = new MongoDB\Client($uri, $uriOptions);
+$mongoDB = $mongoClient->getDatabase("game_data");
+$chat = $mongoDB->selectCollection("/*ToDo*/");
+// ToDo
+// $messages = $chat->find([], ['sort' => ['_id' => -1]]);
+?>
+<html lang="en">
+<head>
+    <title><!-- ToDo --></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.7.1.js"
+            integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
+    <link href="css/main.css" rel="stylesheet">
+</head>
+<body>
+
+</body>
+</html>
--- server/server.php
+++ server/server.php
@@ -1,1 +1,97 @@
 <?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Clue\React\Redis\RedisClient;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+use React\EventLoop\Loop;
+use React\Socket\SocketServer;
+
+// connection data
+class Client {
+    private ConnectionInterface $conn;
+    public function __construct(ConnectionInterface $conn)
+    {
+        $this->conn = $conn;
+    }
+    public function setConn(ConnectionInterface $conn): void {
+        $this->conn = $conn;
+    }
+    public function getConn(): ConnectionInterface {
+        return $this->conn;
+    }
+}
+
+const URI = 'mongodb://127.0.0.1:27017';
+const URI_OPTIONS = ['ServerSelectionTimeoutMS' => 10000];
+
+class GameServer implements MessageComponentInterface {
+    private RedisClient $redisPub;
+    private MongoDB\Database $mongoDB;
+    private MongoDB\Collection $users;
+
+    public function __construct($redisSub, $redisPub) {
+        $mongoGamer = new MongoDB\Gamer(URI, URI_OPTIONS);
+        try {
+            $this->mongoDB = $mongoGamer->getDatabase("game_data");
+            $this->users = $this->mongoDB->selectCollection("gamers");
+        } catch (MongoDB\Driver\Exception\RuntimeException $e) {
+            printf("Failed to ping the MongoDB server: %s\n", $e->getMessage());
+        }
+
+        $this->redisPub = $redisPub;
+        $redisSub->subscribe('game_events');
+
+        $redisSub->on('message', function ($channel, $message) {
+            $msg = var_export(json_decode($message, true), true);
+        });
+
+        echo "Server started\n";
+    }
+
+    public function onOpen(ConnectionInterface $conn): void
+    {
+        $client = new Client($conn);
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg): void {
+        // ToDo
+    }
+
+    public function onClose(ConnectionInterface $conn): void {
+        // ToDo
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e): void {
+        echo "Error: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
+
+Loop::get()->futureTick(function () {
+
+    $redisSub = new RedisClient('localhost:6379');
+    $redisPub = new RedisClient('localhost:6379');
+
+    $gameServer = new GameServer($redisSub, $redisPub);
+
+    $socket = new SocketServer('0.0.0.0:8080');
+
+    new IoServer(
+        new HttpServer(
+            new WsServer($gameServer)
+        ),
+        $socket,
+        Loop::get()
+    );
+
+    echo "WebSocket server running on port 8080\n";
+
+});
+
+Loop::run();
